7ca0ffe74eb333fd0bda4fff83735c5db4175d2e
Thomas Stephens <tmsteph1290@gmail.com>
2024-04-07 15:41:13 +0000

    My own App.js!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import VideoPage from './pages/VideoPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/video/:id" element={<VideoPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,8 @@
+// src/pages/HomePage.js
+import React from 'react';
+
+function HomePage() {
+  return <div>Home Page Content</div>;
+}
+
+export default HomePage;
